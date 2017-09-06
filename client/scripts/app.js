4a4f4f3c90a27f5e31ccbb4e94d5d11aa90cbd77
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-09-06 15:08:12 -0700

    Completed bare minimum requirements for siskel

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,7 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    this.set('like', !this.get('like'));
   }
 
 });
@@ -15,13 +15,16 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', function(movie) {
+      this.sortByField();
+    });
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field || this.comparator;
+    this.sort();
   }
 
 });
@@ -58,7 +61,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.model.on('change', () => { this.render(); });
   },
 
   events: {
@@ -66,7 +69,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +82,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', () => { this.render(); });
   },
 
   render: function() {
